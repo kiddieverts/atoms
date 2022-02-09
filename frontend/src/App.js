1ecb65f7c8e12102a51dfcaa2b5b861ba9a07c07
kiddieverts <kiddieverts@gmail.com>
2022-02-09 21:49:16 +0000

    flatten

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,61 @@ import { Metro } from './components/Metro';
 import { getMelody } from './music/melody-picker';
 import { transformAndPack, transpose } from './music/melody-transform';
 import { pack, unpack } from './utils/packing';
+import { doUnpack } from './music/melody-unpacker-two';
+
+const calculateMelody = (m, melodyId) => {
+  let totalNumberOfBeats = 4 * 4 * 8;
+  let nextStep = {}
+  let playingCount = {}
+  let timeline = [];
+
+  let [pitches, rhythm] = doUnpack(m);
+
+  for (let counter = 0; counter < totalNumberOfBeats; counter++) {
+    let next = nextStep[melodyId];
+
+    const ns = (next === undefined)
+      ? 1
+      : (next === rhythm.length - 1)
+        ? 0
+        : next + 1;
+
+    const pc = next === rhythm.length - 1
+      ? !!playingCount[melodyId]
+        ? playingCount[melodyId] + 1
+        : 1
+      : playingCount[melodyId];
+
+    nextStep[melodyId] = ns;
+    playingCount[melodyId] = pc;
+
+    timeline.push([pitches[ns - 1], rhythm[ns - 1]]);
+  }
+
+  return timeline;
+}
+
+const comb = (melodies) => {
+  const final = [];
+
+  const newArr = calculateMelody(melodies[0], 1);
+
+  for (let j = 0; j < newArr.length; j++) {
+    final.push([]);
+  }
+
+  for (let i = 0; i < melodies.length; i++) {
+    const melody = melodies[i];
+
+    const newArr = calculateMelody(melody, i);
+    for (let j = 0; j < newArr.length; j++) {
+      const bla = [...final[j], newArr[j]];
+      final[j] = bla;
+    }
+  }
+
+  return final;
+}
 
 const App = () => {
   const audioContext = new AudioContext();
@@ -84,47 +139,50 @@ const App = () => {
 
   const handleChangeOct = (v) => setOct(v);
 
+  if (!!finalMelody && !!finalMelody2 && !!finalMelody3 && !!finalMelody4) {
+    const com = comb([finalMelody, finalMelody2, finalMelody3, finalMelody4]);
+    console.log('com', com)
+  }
+
   return <>
-    <>
-      <Metro
-        melodies={melodies}
-        audioContext={audioContext}
-        isStopped={isStopped}
-        tempo={tempo}
-        divider={divider}
-      />
+    <Metro
+      melodies={melodies}
+      audioContext={audioContext}
+      isStopped={isStopped}
+      tempo={tempo}
+      divider={divider}
+    />
 
-      <div>
-        <button className={melody === 1 ? 'selected' : ''} selected onClick={() => handleChangeMelody(1)}></button>
-        <button className={melody === 2 ? 'selected' : ''} onClick={() => handleChangeMelody(2)}></button>
-        <button className={melody === 3 ? 'selected' : ''} onClick={() => handleChangeMelody(3)}></button>
-        <button className={melody === 4 ? 'selected' : ''} onClick={() => handleChangeMelody(4)}></button>
-      </div>
+    <div>
+      <button className={melody === 1 ? 'selected' : ''} selected onClick={() => handleChangeMelody(1)}></button>
+      <button className={melody === 2 ? 'selected' : ''} onClick={() => handleChangeMelody(2)}></button>
+      <button className={melody === 3 ? 'selected' : ''} onClick={() => handleChangeMelody(3)}></button>
+      <button className={melody === 4 ? 'selected' : ''} onClick={() => handleChangeMelody(4)}></button>
+    </div>
 
-      <div>
-        <button className={transNum === 1 ? 'selected' : ''} onClick={() => handleChangeTransformation(1)}></button>
-        <button className={transNum === 2 ? 'selected' : ''} onClick={() => handleChangeTransformation(2)}></button>
-        <button className={transNum === 3 ? 'selected' : ''} onClick={() => handleChangeTransformation(3)}></button>
-        <button className={transNum === 4 ? 'selected' : ''} onClick={() => handleChangeTransformation(4)}></button>
-      </div>
+    <div>
+      <button className={transNum === 1 ? 'selected' : ''} onClick={() => handleChangeTransformation(1)}></button>
+      <button className={transNum === 2 ? 'selected' : ''} onClick={() => handleChangeTransformation(2)}></button>
+      <button className={transNum === 3 ? 'selected' : ''} onClick={() => handleChangeTransformation(3)}></button>
+      <button className={transNum === 4 ? 'selected' : ''} onClick={() => handleChangeTransformation(4)}></button>
+    </div>
 
-      <div>
-        <button className={oct === 1 ? 'selected' : ''} onClick={() => handleChangeOct(1)}></button>
-        <button className={oct === 2 ? 'selected' : ''} onClick={() => handleChangeOct(2)}></button>
-        <button className={oct === 3 ? 'selected' : ''} onClick={() => handleChangeOct(3)}></button>
-        <button className={oct === 4 ? 'selected' : ''} onClick={() => handleChangeOct(4)}></button>
-      </div>
+    <div>
+      <button className={oct === 1 ? 'selected' : ''} onClick={() => handleChangeOct(1)}></button>
+      <button className={oct === 2 ? 'selected' : ''} onClick={() => handleChangeOct(2)}></button>
+      <button className={oct === 3 ? 'selected' : ''} onClick={() => handleChangeOct(3)}></button>
+      <button className={oct === 4 ? 'selected' : ''} onClick={() => handleChangeOct(4)}></button>
+    </div>
 
-      <div>
-        <button className={numberOfMel === 1 ? 'selected' : ''} onClick={() => setNumberOfMel(1)}></button>
-        <button className={numberOfMel === 2 ? 'selected' : ''} onClick={() => setNumberOfMel(2)}></button>
-        <button className={numberOfMel === 3 ? 'selected' : ''} onClick={() => setNumberOfMel(3)}></button>
-        <button className={numberOfMel === 4 ? 'selected' : ''} onClick={() => setNumberOfMel(4)}></button>
-      </div>
+    <div>
+      <button className={numberOfMel === 1 ? 'selected' : ''} onClick={() => setNumberOfMel(1)}></button>
+      <button className={numberOfMel === 2 ? 'selected' : ''} onClick={() => setNumberOfMel(2)}></button>
+      <button className={numberOfMel === 3 ? 'selected' : ''} onClick={() => setNumberOfMel(3)}></button>
+      <button className={numberOfMel === 4 ? 'selected' : ''} onClick={() => setNumberOfMel(4)}></button>
+    </div>
 
-      <button onClick={handleStop}>STOP</button>
-    </>
+    <button onClick={handleStop}>STOP</button>
   </>
-}
 
+}
 export default App;
