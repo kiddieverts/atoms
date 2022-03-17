0bb7b4079df5702d2fde4eb07c5b5f7362dbab4f
kiddieverts <kiddieverts@gmail.com>
2022-03-17 16:07:26 +0000

    convert state to token id and vice versa

--- server/vanilla/atoms-nft/index.js
+++ server/vanilla/atoms-nft/index.js
@@ -3,7 +3,8 @@ import { getIdFromParam, idToState } from './utils/misc.js';
 import { pack, unpack } from './utils/packing.js';
 import { applyToAllVoices } from './utils/applyToAllVoices.js';
 import { renderPads } from './pads.js';
+import { convertToState } from './utils/tokenIdToState.js';
 
 // module.exports = sketch;
-export { getIdFromParam, idToState, pack, unpack, applyToAllVoices, renderPads };
+export { getIdFromParam, idToState, pack, unpack, applyToAllVoices, renderPads, convertToState };
 export default atoms;
