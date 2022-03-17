601990f6e901c1baacd5346c8fa06b3c3f30ec60
kiddieverts <kiddieverts@gmail.com>
2022-03-17 10:21:05 +0000

    cleanup

--- server/vanilla/atoms-nft/utils/generateVoices.js
+++ server/vanilla/atoms-nft/utils/generateVoices.js
@@ -1,4 +1,4 @@
-import { combineMelodies } from './combineMelodies.js';
+import { unpack } from '../utils/packing.js';
 
 export const generateVoices = (patch, scale, state, totalNumberOfBeats) => {
   const { melodies, tempo } = runPatch(state, patch, scale);
@@ -25,4 +25,61 @@ const runPatch = (state, patch, scale) => {
 
   return fnSix(fnFive(fnFour(fnThree(fnTwo(startWith)))));
 }
+
+const combineMelodies = (melodies, totalNumberOfBeats) => {
+  const combined = [];
+
+  for (let i = 0; i < totalNumberOfBeats; i++) {
+    combined.push([]);
+  }
+
+  for (let i = 0; i < melodies.length; i++) {
+    const melody = melodies[i];
+    const newArr = calculateVoice(melody, i, totalNumberOfBeats);
+
+    for (let j = 0; j < newArr.length; j++) {
+      combined[j] = [...combined[j], newArr[j]];
+    }
+  }
+
+  return combined;
+}
+
+const calculateVoice = (m, melodyId, totalNumberOfBeats) => {
+  let nextStep = {}
+  const timeline = [];
+
+  const [pitches, rhythm] = doUnpack(m);
+
+  for (let counter = 0; counter < totalNumberOfBeats; counter++) {
+    let next = nextStep[melodyId];
+
+    const ns = next === undefined
+      ? 1
+      : next === rhythm.length - 1
+        ? 0
+        : next + 1;
+
+    nextStep[melodyId] = ns;
+
+    timeline.push([pitches[ns - 1], rhythm[ns - 1]]);
+  }
+
+  return timeline;
+}
+
+const doUnpack = (melody) => {
+  const arr = [];
+  for (let i = 0; i < melody.length; i++) {
+    const [p, r] = melody[i];
+    const repeats = r - 1;
+
+    arr.push([p, r]);
+    for (let j = 0; j < repeats; j++) {
+      arr.push([null, null]);
+    }
+  }
+  return unpack(arr);
+}
+
 
