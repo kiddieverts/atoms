46b6ff6e5b5fddd074ca528998d19edcc1933913
kiddieverts <kiddieverts@gmail.com>
2022-03-17 11:42:48 +0000

    rename file

--- server/vanilla/atoms-nft/index.js
+++ server/vanilla/atoms-nft/index.js
@@ -2,7 +2,7 @@ import atoms from './atoms.js';
 import { getIdFromParam, idToState } from './utils/misc.js';
 import { pack, unpack } from './utils/packing.js';
 import { applyToAllVoices } from './utils/applyToAllVoices.js';
-import { renderPads } from './mint.js';
+import { renderPads } from './pads.js';
 
 // module.exports = sketch;
 export { getIdFromParam, idToState, pack, unpack, applyToAllVoices, renderPads };
